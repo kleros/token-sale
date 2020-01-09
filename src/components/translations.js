3597cba33e4d0b3388ae950eef949edd1fbc31a7
Hazelstar <45568911+Hazelstar@users.noreply.github.com>
2020-01-09 14:06:08 +0000

    Update translations.js
    I added the FR translation of the token sale page below the EN one.

--- src/components/translations.js
+++ src/components/translations.js
@@ -27,7 +27,7 @@ export default {
         unlock: 'Unlock Metamask to contribute'
       },
       totals: {
-        title: 'A total of 160 Million of Pinakion (PNK) will be sold over the course of the next (two) week(s).',
+        title: 'A total of 150 Million of Pinakion (PNK) will be sold over the course of the next (two) week(s).',
         amountForSale: 'Amount For Sale',
         totalSold: 'TOTAL PNK Sold',
         remaining: 'Remaining amount for sale'
@@ -38,6 +38,47 @@ export default {
       sellOrder: 'Sell Order',
       price: 'Price (ETH)',
       amount: 'Amount (PNK)'
+    }
+  }
+}
+                     
+"fr": {
+    header: {
+      learnMore: "En savoir plus sur Kleros",
+      title: "Vente de Jetons Kleros",
+      dates: "Début : 11 janvier 2020. Fin : 1er février 2020"
+    },
+    body: {
+      title: "Vente de PNK Kleros",
+      contribute: "Comment contribuer",
+      basicHeading: "Méthode simple",
+      basic: {
+        title: (<>Envoyez vos ETH directement à l adresse suivante<sup>*</sup></>),
+        copyContrib: "Copier l'adresse de contribution",
+        copied: "Copiée !",
+        disclaimer: (<><sup>*</sup>Les ordres les plus bas sont remplis en premier</>)
+      },
+      web3Heading: "Méthode Web3",
+      web3: {
+        title: "Faire une transaction depuis un porte-monnaie Web3",
+        maxPriceLabel: "Prix maximum par PNK",
+        contributeLabel: "ETH en contribution",
+        total: "Total",
+        contribute: "Contribuer",
+        unlock: "Déverrouiller MetaMask pour contribuer"
+      },
+      totals: {
+        title: "Au total, 150 Million de Pinakion (PNK) seront mis en vente au cours de la (des deux) prochaine(s) semaine(s).",
+        amountForSale: "Quantité mise en vente",
+        totalSold: "Quantité vendue",
+        remaining: "Quantité restant à vendre"
+      }
+    },
+    orders: {
+      title: "Les Ordres de Vente",
+      sellOrder: "Ordre de vente",
+      price: "Prix (ETH)",
+      amount: "Quantité (PNK)"
     }
   }
 }
