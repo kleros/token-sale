ce8a3e6417ddf81959c51dbb0ddfc5f686a6d669
Ferit Tunçer <ferit@cryptolab.net>
2020-01-10 20:15:50 +0300

    refactor(footer): internationalize

--- src/bootstrap/app.js
+++ src/bootstrap/app.js
@@ -1,35 +1,35 @@
-import '../styles/theme.css'
-import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom'
-import { Col, Layout, Menu, Row, Select } from 'antd'
-import { DrizzleProvider, Initializer } from '../temp/drizzle-react-hooks'
-import { Helmet } from 'react-helmet'
-import Home from '../containers/home'
-import { ReactComponent as Logo } from '../assets/images/logo.svg'
-import { StyledText } from '../components/typography'
-import React, { useState } from 'react'
-import drizzle from './drizzle'
-import { register } from './service-worker'
-import styled from 'styled-components/macro'
-import media from 'styled-media-query'
-import { default as Footer } from '@kleros/react-components/dist/footer'
-import Translations from '../components/translations'
+import "../styles/theme.css";
+import { BrowserRouter, NavLink, Route, Switch } from "react-router-dom";
+import { Col, Layout, Menu, Row, Select } from "antd";
+import { DrizzleProvider, Initializer } from "../temp/drizzle-react-hooks";
+import { Helmet } from "react-helmet";
+import Home from "../containers/home";
+import { ReactComponent as Logo } from "../assets/images/logo.svg";
+import { StyledText } from "../components/typography";
+import React, { useState } from "react";
+import drizzle from "./drizzle";
+import { register } from "./service-worker";
+import styled from "styled-components/macro";
+import media from "styled-media-query";
+import { default as Footer } from "@kleros/react-components/dist/footer";
+import Translations from "../components/translations";
 
 const StyledLogoCol = styled(Col)`
   align-items: center;
   display: flex;
   height: 60px;
   justify-content: space-evenly;
-`
+`;
 
 const StyledLeftCol = styled(Col)`
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
-  ${media.lessThan('768px')`
+  ${media.lessThan("768px")`
     /* screen width is less than 768px (medium) */
     display: none;
   `}
-`
+`;
 
 const StyledColRight = styled(Col)`
   align-items: right;
@@ -40,11 +40,11 @@ const StyledColRight = styled(Col)`
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
-  ${media.lessThan('992px')`
+  ${media.lessThan("992px")`
     /* screen width is less than 768px (medium) */
     display: none;
   `}
-`
+`;
 const StyledLanguagesCol = styled(Col)`
   align-items: right;
   display: flex;
@@ -52,20 +52,20 @@ const StyledLanguagesCol = styled(Col)`
   justify-content: space-evenly;
   white-space: nowrap;
   overflow: hidden;
-`
+`;
 
 const StyledMenu = styled(Menu)`
   line-height: 60px !important;
   text-align: center;
-`
+`;
 const StyledLayoutContent = styled(Layout.Content)`
   background-color: transparent;
   padding: 0px 7.575vw 100px 7.575vw;
-`
+`;
 const StyledLayout = styled(Layout)`
   min-height: 100vh;
   overflow-x: hidden;
-`
+`;
 const StyledSelect = styled(Select)`
   .ant-select-selection {
     background: rgba(255, 255, 255, 0.1);
@@ -83,20 +83,20 @@ const StyledSelect = styled(Select)`
 
   width: 160px;
   margin-top: 15px;
-`
+`;
 
 const getDefaultLanguage = () => {
   let language = (
     window.navigator.userLanguage ||
     window.navigator.language ||
-    'en'
-  ).split('-')[0]
-  if (!Translations[language]) language = 'en'
-  return language
-}
+    "en"
+  ).split("-")[0];
+  if (!Translations[language]) language = "en";
+  return language;
+};
 
 export default () => {
-  const [language, setLanguage] = useState(getDefaultLanguage())
+  const [language, setLanguage] = useState(getDefaultLanguage());
 
   return (
     <>
@@ -114,7 +114,7 @@ export default () => {
               <Layout.Header>
                 <Row>
                   <StyledLogoCol lg={3} md={6} sm={12} xs={12}>
-                    <a href="https://kleros.io" style={{ marginTop: '20px' }}>
+                    <a href="https://kleros.io" style={{ marginTop: "20px" }}>
                       <Logo />
                     </a>
                   </StyledLogoCol>
@@ -132,7 +132,7 @@ export default () => {
                     <StyledSelect
                       defaultValue={language}
                       onChange={val => {
-                        setLanguage(val)
+                        setLanguage(val);
                       }}
                     >
                       <Select.Option value="en">English</Select.Option>
@@ -159,13 +159,14 @@ export default () => {
                 contractExplorerURL={`https://etherscan.io/address/${
                   process.env.REACT_APP_KLEROS_TEST_SALE_ADDRESS
                 }#code`}
+                locale={language}
               />
             </StyledLayout>
           </BrowserRouter>
         </Initializer>
       </DrizzleProvider>
     </>
-  )
-}
+  );
+};
 
-register()
+register();
