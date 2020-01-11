7fc8442a487efe232199b00000da2d9a8f7fb8f7
Ferit Tunçer <ferit@cryptolab.net>
2020-01-11 22:30:16 +0300

    refactor(intl): improve tr gas comment

--- src/components/translations.js
+++ src/components/translations.js
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from "react";
 
 export default {
   en: {
     header: {
-      learnMore: 'Learn More About Kleros',
-      title: 'Kleros Token Sale',
-      dates: 'Start: January 11th 2020. End: February 11th 2020'
+      learnMore: "Learn More About Kleros",
+      title: "Kleros Token Sale",
+      dates: "Start: January 11th 2020. End: February 11th 2020"
     },
     body: {
-      title: 'Kleros PNK Sale',
-      contribute: 'How to Buy',
-      basicHeading: 'Simple',
+      title: "Kleros PNK Sale",
+      contribute: "How to Buy",
+      basicHeading: "Simple",
       basic: {
         title: (
           <>
@@ -18,48 +18,48 @@ export default {
             address<sup>*</sup>. Please provide at least 300 000 gas.
           </>
         ),
-        copyContrib: 'Copy Contribution Address',
-        copied: 'Copied!',
+        copyContrib: "Copy Contribution Address",
+        copied: "Copied!",
         disclaimer: (
           <>
             <sup>*</sup>Lowest priced orders will be filled first.
           </>
         )
       },
-      web3Heading: 'Advanced',
+      web3Heading: "Advanced",
       web3: {
-        title: 'Make a transaction with a web3 wallet',
+        title: "Make a transaction with a web3 wallet",
         maxPriceLabel: "What's the max price you want to pay per PNK?",
-        contributeLabel: 'How much ETH do you want to contribute?',
-        total: 'Total',
-        contribute: 'Contribute',
-        unlock: 'Unlock Metamask to contribute'
+        contributeLabel: "How much ETH do you want to contribute?",
+        total: "Total",
+        contribute: "Contribute",
+        unlock: "Unlock Metamask to contribute"
       },
       totals: {
         title:
-          'A total of 150 Million PNK will be put on sale from January 11 to February 11, 2020',
-        amountForSale: 'Amount For Sale',
-        totalSold: 'Total Sold',
-        remaining: 'Remaining for Sale'
+          "A total of 150 Million PNK will be put on sale from January 11 to February 11, 2020",
+        amountForSale: "Amount For Sale",
+        totalSold: "Total Sold",
+        remaining: "Remaining for Sale"
       }
     },
     orders: {
-      title: 'Sell Orders',
-      sellOrder: 'Sell Order',
-      price: 'Price (ETH)',
-      amount: 'Amount (PNK)'
+      title: "Sell Orders",
+      sellOrder: "Sell Order",
+      price: "Price (ETH)",
+      amount: "Amount (PNK)"
     }
   },
   fr: {
     header: {
-      learnMore: 'En savoir plus sur Kleros',
-      title: 'Vente de Jetons Kleros',
-      dates: 'Début : 11 janvier 2020. Fin : 11 février 2020'
+      learnMore: "En savoir plus sur Kleros",
+      title: "Vente de Jetons Kleros",
+      dates: "Début : 11 janvier 2020. Fin : 11 février 2020"
     },
     body: {
-      title: 'Vente de PNK Kleros',
-      contribute: 'Comment Contribuer',
-      basicHeading: 'Méthode Simple',
+      title: "Vente de PNK Kleros",
+      contribute: "Comment Contribuer",
+      basicHeading: "Méthode Simple",
       basic: {
         title: (
           <>
@@ -68,48 +68,48 @@ export default {
           </>
         ),
         copyContrib: "Copier l'adresse",
-        copied: 'Copiée !',
+        copied: "Copiée !",
         disclaimer: (
           <>
             <sup>*</sup>Les ordres les plus bas sont remplis en premier
           </>
         )
       },
-      web3Heading: 'Méthode Avancée',
+      web3Heading: "Méthode Avancée",
       web3: {
-        title: 'Faire une transaction depuis un porte-monnaie Web3',
+        title: "Faire une transaction depuis un porte-monnaie Web3",
         maxPriceLabel:
-          'Quel est le prix maximum que vous souhaitez payer par PNK ?',
+          "Quel est le prix maximum que vous souhaitez payer par PNK ?",
         contributeLabel: "De combien d'ETH souhaitez-vous contribuer ?",
-        total: 'Total',
-        contribute: 'Contribuer',
-        unlock: 'Déverrouiller MetaMask pour contribuer'
+        total: "Total",
+        contribute: "Contribuer",
+        unlock: "Déverrouiller MetaMask pour contribuer"
       },
       totals: {
         title:
-          'Au total, 150 Million de Pinakion (PNK) seront mis en vente au cours du mois.',
-        amountForSale: 'Quantité mise en vente',
-        totalSold: 'Quantité vendue',
-        remaining: 'Quantité restant à vendre'
+          "Au total, 150 Million de Pinakion (PNK) seront mis en vente au cours du mois.",
+        amountForSale: "Quantité mise en vente",
+        totalSold: "Quantité vendue",
+        remaining: "Quantité restant à vendre"
       }
     },
     orders: {
-      title: 'Les Ordres de Vente',
-      sellOrder: 'Ordre de vente',
-      price: 'Prix (ETH)',
-      amount: 'Quantité (PNK)'
+      title: "Les Ordres de Vente",
+      sellOrder: "Ordre de vente",
+      price: "Prix (ETH)",
+      amount: "Quantité (PNK)"
     }
   },
   pt: {
     header: {
-      learnMore: 'Saiba mais sobre o Kleros',
-      title: 'Venda de Tokens Kleros ',
-      dates: ' Início: 11 de Janeiro de 2020 Final: 11 de Fevereiro de 2020'
+      learnMore: "Saiba mais sobre o Kleros",
+      title: "Venda de Tokens Kleros ",
+      dates: " Início: 11 de Janeiro de 2020 Final: 11 de Fevereiro de 2020"
     },
     body: {
-      title: 'Venda de PNK Kleros',
-      contribute: 'Como Contribuir',
-      basicHeading: 'Básico',
+      title: "Venda de PNK Kleros",
+      contribute: "Como Contribuir",
+      basicHeading: "Básico",
       basic: {
         title: (
           <>
@@ -117,8 +117,8 @@ export default {
             Forneça pelo menos 300 000 gas.
           </>
         ),
-        copyContrib: 'Copie o Endereço',
-        copied: 'Copiado!',
+        copyContrib: "Copie o Endereço",
+        copied: "Copiado!",
         disclaimer: (
           <>
             <sup>*</sup>A contribuição preenche os pedidos mais baratos
@@ -126,40 +126,40 @@ export default {
           </>
         )
       },
-      web3Heading: 'Avançado',
+      web3Heading: "Avançado",
       web3: {
-        title: 'Faça a transação com uma carteira Web3',
-        maxPriceLabel: 'Qual o preço máximo que você deseja pagar por PNK?',
-        contributeLabel: 'Quantos ETH você deseja contribuir?',
-        total: 'Total',
-        contribute: 'Contribua',
-        unlock: 'Desbloqueie o Metamask para contribuir'
+        title: "Faça a transação com uma carteira Web3",
+        maxPriceLabel: "Qual o preço máximo que você deseja pagar por PNK?",
+        contributeLabel: "Quantos ETH você deseja contribuir?",
+        total: "Total",
+        contribute: "Contribua",
+        unlock: "Desbloqueie o Metamask para contribuir"
       },
       totals: {
         title:
-          'Um total de 150 milhões de PNK serão colocados a venda entre 11 de janeiro e 11 de fevereiro de 2020',
-        amountForSale: 'Quantidade à venda',
-        totalSold: 'Total Vendido',
-        remaining: 'Quantidade restante à venda'
+          "Um total de 150 milhões de PNK serão colocados a venda entre 11 de janeiro e 11 de fevereiro de 2020",
+        amountForSale: "Quantidade à venda",
+        totalSold: "Total Vendido",
+        remaining: "Quantidade restante à venda"
       }
     },
     orders: {
-      title: 'Ordens de Venda',
-      sellOrder: 'Ordem de Venda',
-      price: 'Preço (ETH)',
-      amount: 'Quantidade (PNK)'
+      title: "Ordens de Venda",
+      sellOrder: "Ordem de Venda",
+      price: "Preço (ETH)",
+      amount: "Quantidade (PNK)"
     }
   },
   es: {
     header: {
-      learnMore: 'Learn More About Kleros',
-      title: 'Venta de Token de Kleros',
-      dates: '11 Ene 2020 a 11 Feb 2020'
+      learnMore: "Learn More About Kleros",
+      title: "Venta de Token de Kleros",
+      dates: "11 Ene 2020 a 11 Feb 2020"
     },
     body: {
-      title: 'Venta de PNK de Kleros',
-      contribute: 'Cómo Contribuir',
-      basicHeading: 'Simple',
+      title: "Venta de PNK de Kleros",
+      contribute: "Cómo Contribuir",
+      basicHeading: "Simple",
       basic: {
         title: (
           <>
@@ -167,97 +167,97 @@ export default {
             al menos 300 000 unidades de gas
           </>
         ),
-        copyContrib: 'Copiar Dirección',
-        copied: 'Copied!',
+        copyContrib: "Copiar Dirección",
+        copied: "Copied!",
         disclaimer: (
           <>
             <sup>*</sup>Las órdenes de menor precio se llenan primero.
           </>
         )
       },
-      web3Heading: 'Avanzado',
+      web3Heading: "Avanzado",
       web3: {
-        title: 'Hacer transacción con wallet web3',
-        maxPriceLabel: '¿Cuál es el precio máximo que quieres pagar por PNK?',
-        contributeLabel: '¿Cuánto ETH quieres contribuir?',
-        total: 'Total',
-        contribute: 'Contribuir',
-        unlock: 'Desbloquear Metamask para contribuir'
+        title: "Hacer transacción con wallet web3",
+        maxPriceLabel: "¿Cuál es el precio máximo que quieres pagar por PNK?",
+        contributeLabel: "¿Cuánto ETH quieres contribuir?",
+        total: "Total",
+        contribute: "Contribuir",
+        unlock: "Desbloquear Metamask para contribuir"
       },
       totals: {
-        title: 'Un total de 150 millones de Pinakion (PNK) serán vendidos.',
-        amountForSale: 'Cantidad a Vender',
-        totalSold: 'Cantidad Vendida',
-        remaining: 'Cantidad Restante a Vender'
+        title: "Un total de 150 millones de Pinakion (PNK) serán vendidos.",
+        amountForSale: "Cantidad a Vender",
+        totalSold: "Cantidad Vendida",
+        remaining: "Cantidad Restante a Vender"
       }
     },
     orders: {
-      title: 'Órdenes de Venta',
-      sellOrder: 'Orden de Venta',
-      price: 'Precio (ETH)',
-      amount: 'Cantidad (PNK)'
+      title: "Órdenes de Venta",
+      sellOrder: "Orden de Venta",
+      price: "Precio (ETH)",
+      amount: "Cantidad (PNK)"
     }
   },
   tr: {
     header: {
-      learnMore: 'Kleros hakkında detaylı bilgi için',
-      title: 'Kleros Jeton Satışı',
-      dates: 'Başlangıç: 11 Ocak 2020. Bitiş: 11 Şubat 2020'
+      learnMore: "Kleros hakkında detaylı bilgi için",
+      title: "Kleros Jeton Satışı",
+      dates: "Başlangıç: 11 Ocak 2020. Bitiş: 11 Şubat 2020"
     },
     body: {
-      title: 'Kleros PNK Satışı',
-      contribute: 'PNK Satın Al',
-      basicHeading: 'Basit',
+      title: "Kleros PNK Satışı",
+      contribute: "PNK Satın Al",
+      basicHeading: "Basit",
       basic: {
         title: (
           <>
-            Bu adrese ETH göndermeniz yeterli<sup>*</sup>. Lütfen en az 300 000
-            gaz sağlayın
+            Bu adrese ETH göndermeniz (lütfen borsalardan göndermeyin) yeterli
+            <sup>*</sup>. Lütfen, işlem için en az 300000 gaz kullanın.
           </>
         ),
-        copyContrib: 'Adresi Kopyala',
-        copied: 'Kopyalandı!',
+        copyContrib: "Adresi Kopyala",
+        copied: "Kopyalandı!",
         disclaimer: (
           <>
             <sup>*</sup>Mümkün olan en düşük fiyattan satın alınacaktır
           </>
         )
       },
-      web3Heading: 'Gelişmiş',
+      web3Heading: "Gelişmiş",
       web3: {
-        title: 'Web3 cüzdanınız ile bir işlem yapın',
+        title: "Web3 cüzdanınız ile bir işlem yapın",
         maxPriceLabel:
-          'Bir adet PNK jetonu için ödemek istediğiniz azami fiyat nedir?',
-        contributeLabel: 'Ne kadar ETH karşılığı satın almak istiyorsunuz?',
-        total: 'Toplam',
-        contribute: 'Satın Al',
+          "Bir adet PNK jetonu için ödemek istediğiniz azami fiyat nedir?",
+        contributeLabel: "Ne kadar ETH karşılığı satın almak istiyorsunuz?",
+        total: "Toplam",
+        contribute: "Satın Al",
         unlock: "Satın alabilmek için Metamask'i açın"
       },
       totals: {
         title:
-          '11 Ocak ve 11 Şubat arasında toplam 150 Milyon jeton satılacaktır',
-        amountForSale: 'Satışa Çıkarılan Toplam Miktar',
-        totalSold: 'Satılan Miktar',
-        remaining: 'Satılmayı Bekleyen Miktar'
+          "11 Ocak ve 11 Şubat arasında toplam 150 Milyon jeton satılacaktır",
+        amountForSale: "Satışa Çıkarılan Toplam Miktar",
+        totalSold: "Satılan Miktar",
+        remaining: "Satılmayı Bekleyen Miktar"
       }
     },
     orders: {
-      title: 'Satış Emirleri',
-      sellOrder: 'Satış Emri',
-      price: 'Fiyat (ETH)',
-      amount: 'Miktar (PNK)'
+      title: "Satış Emirleri",
+      sellOrder: "Satış Emri",
+      price: "Fiyat (ETH)",
+      amount: "Miktar (PNK)"
     }
   },
   ru: {
     header: {
-      learnMore: 'Узнайте больше о Kleros',
-      title: 'Продажа токенов Kleros',
-      dates: 'Начало: 11 января 2020. Окончание: 11 февраля 2020'
+      learnMore: "Узнайте больше о Kleros",
+      title: "Продажа токенов Kleros",
+      dates: "Начало: 11 января 2020. Окончание: 11 февраля 2020"
     },
     body: {
-      title: 'Продажа Kleros PNK',
-      contribute: 'Как совершить покупку',
-      basicHeading: 'Основной',
+      title: "Продажа Kleros PNK",
+      contribute: "Как совершить покупку",
+      basicHeading: "Основной",
       basic: {
         title: (
           <>
@@ -265,49 +265,49 @@ export default {
             Пожалуйста, предоставьте не менее 300 000 газов.
           </>
         ),
-        copyContrib: 'Скопировать адрес',
-        copied: 'Скопировано!',
+        copyContrib: "Скопировать адрес",
+        copied: "Скопировано!",
         disclaimer: (
           <>
             <sup>*</sup>Заказы с наименьшей ценой будут выполнены первыми.
           </>
         )
       },
-      web3Heading: 'Продвинутый',
+      web3Heading: "Продвинутый",
       web3: {
-        title: 'Осуществите транзакцию с помощью web3 кошелька',
-        maxPriceLabel: 'Какую максимальную цену вы хотите заплатить за PNK?',
-        contributeLabel: 'Сколько ETH вы хотите вложить?',
-        total: 'Всего',
-        contribute: 'Вложить',
-        unlock: 'Разблокируйте Metamask чтобы осуществить вложение'
+        title: "Осуществите транзакцию с помощью web3 кошелька",
+        maxPriceLabel: "Какую максимальную цену вы хотите заплатить за PNK?",
+        contributeLabel: "Сколько ETH вы хотите вложить?",
+        total: "Всего",
+        contribute: "Вложить",
+        unlock: "Разблокируйте Metamask чтобы осуществить вложение"
       },
       totals: {
         title:
-          'Всего 150 миллионов Пинакинов (PNK) будет продано в течение следующего месяца',
-        amountForSale: 'Количество на продажу',
-        totalSold: 'Продано',
-        remaining: 'Осталось продать'
+          "Всего 150 миллионов Пинакинов (PNK) будет продано в течение следующего месяца",
+        amountForSale: "Количество на продажу",
+        totalSold: "Продано",
+        remaining: "Осталось продать"
       }
     },
     orders: {
-      title: 'Заказы на Продажу',
-      sellOrder: 'Заказ на Продажу',
-      price: 'Цена (ETH)',
-      amount: 'Количество (PNK)'
+      title: "Заказы на Продажу",
+      sellOrder: "Заказ на Продажу",
+      price: "Цена (ETH)",
+      amount: "Количество (PNK)"
     }
   },
   kr: {
     header: {
-      learnMore: '클레로스에 대해 더 알아보기',
-      title: '클레로스 토큰 판매',
-      dates: '개시일: 2020년 1월 11일 (UTC+0) 종료일: 2020년 2월 11일 (UTC+0)'
+      learnMore: "클레로스에 대해 더 알아보기",
+      title: "클레로스 토큰 판매",
+      dates: "개시일: 2020년 1월 11일 (UTC+0) 종료일: 2020년 2월 11일 (UTC+0)"
     },
 
     body: {
-      title: '클레로스 PNK 판매',
-      contribute: '구매 방식',
-      basicHeading: '단순 방식',
+      title: "클레로스 PNK 판매",
+      contribute: "구매 방식",
+      basicHeading: "단순 방식",
       basic: {
         title: (
           <>
@@ -315,36 +315,36 @@ export default {
             사용해주세요) <sup>*</sup>. 최소 300,000 개의 가스를 제공하십시오
           </>
         ),
-        copyContrib: '구매 주소 복사',
-        copied: '복사 완료!',
+        copyContrib: "구매 주소 복사",
+        copied: "복사 완료!",
         disclaimer: (
           <>
             <sup>*</sup>가장 낮은 가격 대의 주문부터 먼저 체결됩니다.
           </>
         )
       },
-      web3Heading: '심화 방식',
+      web3Heading: "심화 방식",
       web3: {
-        title: '웹3 지갑으로 전송합니다.',
-        maxPriceLabel: 'PNK 구매 가격 선택',
-        contributeLabel: 'ETH 전송량?',
-        total: '총액',
-        contribute: '전송',
-        unlock: '전송을 위해 MetaMask에 접속해주세요.'
+        title: "웹3 지갑으로 전송합니다.",
+        maxPriceLabel: "PNK 구매 가격 선택",
+        contributeLabel: "ETH 전송량?",
+        total: "총액",
+        contribute: "전송",
+        unlock: "전송을 위해 MetaMask에 접속해주세요."
       },
       totals: {
         title:
-          '2020년 1월 11일부터 2020년 2월 11일까지 1억 5천만개의 PNK가 판매될 것입니다.',
-        amountForSale: '총 판매 예정량',
-        totalSold: '현재까지의 판매량',
-        remaining: '남은 판매량'
+          "2020년 1월 11일부터 2020년 2월 11일까지 1억 5천만개의 PNK가 판매될 것입니다.",
+        amountForSale: "총 판매 예정량",
+        totalSold: "현재까지의 판매량",
+        remaining: "남은 판매량"
       }
     },
     orders: {
-      title: '판매 주문',
-      sellOrder: '판매 주문',
-      price: '가격 (ETH)',
-      amount: '양 (PNK)'
+      title: "판매 주문",
+      sellOrder: "판매 주문",
+      price: "가격 (ETH)",
+      amount: "양 (PNK)"
     }
   }
-}
+};
