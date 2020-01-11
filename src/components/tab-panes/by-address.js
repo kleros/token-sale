501f001128acabcc017a67fc299bd480a49cde07
Ferit Tunçer <ferit@cryptolab.net>
2020-01-11 13:17:41 +0300

    fix(by-address): overall responsiveness

--- src/components/tab-panes/by-address.js
+++ src/components/tab-panes/by-address.js
@@ -1,21 +1,23 @@
-import { Button } from 'antd'
-import Copy from 'copy-to-clipboard'
-import QRCode from 'qrcode.react' // TODO should use hardcoded image for security?
-import React, { useState } from 'react'
-import styled from 'styled-components/macro'
-import Translations from '../translations'
+import { Button } from "antd";
+import Copy from "copy-to-clipboard";
+import QRCode from "qrcode.react"; // TODO should use hardcoded image for security?
+import React, { useState } from "react";
+import styled from "styled-components/macro";
+import Translations from "../translations";
 
-import { StyledText, StyledValueText } from '../typography'
+import { StyledText, StyledValueText } from "../typography";
 
 const StyledPane = styled.div`
   text-align: center;
-`
+  padding: 0px 1vw 0px 1vw;
+`;
 
 const StyledButton = styled(Button)`
   color: white;
   height: 40px;
-  width: 66%;
-  padding: 0px 33px;
+  width: 100%;
+  padding-left: 1rem;
+  padding-right: 1rem;
   &:focus {
     color: white;
   }
@@ -30,7 +32,7 @@ const StyledButton = styled(Button)`
   @media (max-width: 991px) {
     font-size: 14px;
   }
-`
+`;
 
 const AddressResponsive = styled(StyledValueText)`
   font-size: 1vw;
@@ -39,30 +41,29 @@ const AddressResponsive = styled(StyledValueText)`
   @media (max-width: 991px) {
     font-size: 14px;
   }
-`
+`;
 
-const ByAddressPane = ({
-  contributionAddress,
-  language
-}) => {
+const ByAddressPane = ({ contributionAddress, language }) => {
   const [state, setState] = useState({
     buttonText: Translations[language].body.basic.copyContrib
-  })
+  });
 
   const CopyToClipboard = () => {
-    Copy(contributionAddress)
+    Copy(contributionAddress);
     setState({
       buttonText: Translations[language].body.basic.copied
-    })
+    });
 
-    setTimeout(function(){ setState({
-      buttonText: Translations[language].body.basic.copyContrib
-    }) }, 500);
-  }
+    setTimeout(function() {
+      setState({
+        buttonText: Translations[language].body.basic.copyContrib
+      });
+    }, 500);
+  };
 
   return (
     <StyledPane>
-      <StyledText style={{'marginTop' : '30px', 'marginBottom' : '34px'}}>
+      <StyledText style={{ marginTop: "30px", marginBottom: "34px" }}>
         {Translations[language].body.basic.title}
       </StyledText>
       <QRCode
@@ -72,9 +73,11 @@ const ByAddressPane = ({
       />
       <AddressResponsive>{contributionAddress}</AddressResponsive>
       <StyledButton onClick={CopyToClipboard}>{state.buttonText}</StyledButton>
-      <StyledText style={{'padding': '0px 54px', 'marginTop': '55px'}}>{Translations[language].body.basic.disclaimer}</StyledText>
+      <StyledText style={{ padding: "0px 1rem", marginTop: "55px" }}>
+        {Translations[language].body.basic.disclaimer}
+      </StyledText>
     </StyledPane>
-  )
-}
+  );
+};
 
-export default ByAddressPane
+export default ByAddressPane;
